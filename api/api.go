962f5d19f00348eb5f1447fef87149e8b3919c9f
Raphael <raphael@charonne.com>
2017-09-06 10:16:57 +0200

    debug

--- api/api.go
+++ api/api.go
@@ -111,17 +111,17 @@ func ContractExec(execData ContractExecData) map[string]interface{} {
 	db := database.DatabaseConnection()
 
 	// Get contract
-	var contractDeployed database.ContractDeployed
-	if err := db.First(&contractDeployed, "Address = ?", execData.Address).Error; err != nil {
-		// Response data
-		responseData := map[string]interface{}{
-			"status": "error",
-			"address": execData.Address,
-			"message": "contract not found",
-		}
-		return responseData
-  }
-	
+	// var contractDeployed database.ContractDeployed
+	// if err := db.First(&contractDeployed, "Address = ?", execData.Address).Error; err != nil {
+	// 	// Response data
+	// 	responseData := map[string]interface{}{
+	// 		"status": "error",
+	// 		"address": execData.Address,
+	// 		"message": "contract not found",
+	// 	}
+	// 	return responseData
+  // }
+
 	// Exec contract
 	txhash := ethereum.Exec(execData.Address, execData.Method, execData.Params)
 
